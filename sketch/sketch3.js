9bdcc15451a8383fb69abf5bbb82acb2679609c1
unknown <pedro.ze.lagarto@gmail.com>
2018-03-29 01:20:19 +0100

    aspect ratios..

---
+++ sketch/sketch3.js
@@ -0,0 +1,62 @@
+/*
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame       || 
+		window.webkitRequestAnimationFrame || 
+		window.mozRequestAnimationFrame    || 
+		window.oRequestAnimationFrame      || 
+		window.msRequestAnimationFrame     || 
+		function( callback ){
+			window.setTimeout(callback, 1000 / 60);
+		};
+})();
+*/
+
+var canvas = document.getElementById("sketch");
+var	ctx = canvas.getContext("2d");
+var	mouse = {
+	x : -1,
+	y : -1
+};
+
+canvas.width = 1024;
+canvas.height = 516;
+
+ctx.font = '48px serif';
+
+document.addEventListener("mousemove", function(e) {
+		var rect = canvas.getBoundingClientRect();
+		var clippedX = e.pageX - rect.left;
+		var clippedY = e.pageY - rect.top;
+
+		if (clippedX < 0) {
+			clippedX = -1;
+		}else if (e.pageX > rect.right) {
+			clippedX = -1;
+		}
+
+		if (clippedY < 0) {
+			clippedY = -1;
+		}else if (e.pageY > rect.bottom) {
+			clippedY = -1;
+		}
+
+		if ( (clippedX === -1) || (clippedY === -1) ) {
+			clippedX = -1;
+			clippedY = -1;
+		}
+
+		mouse.x = Math.round(clippedX);
+		mouse.y = Math.round(clippedY);
+}, false);
+
+(function animloop(){
+	window.requestAnimationFrame(animloop);
+	update();
+})();
+
+function update() {
+	var mousePos = '(' + mouse.x + ',' + mouse.y + ')';
+	//ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.fillText('Mouse Pos : ' + mousePos, 10, 50);
+}
